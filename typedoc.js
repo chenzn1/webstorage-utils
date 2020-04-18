506280404f98f16104add069ef9a2b33fb4b063f
魔鬼筋肉人 <312073339@qq.com>
2020-04-18 22:25:01 +0800

    build: add typedoc and travis config (#1)
    * build: add typedoc and travis config
    
    * chore: add .travis.yml
    
    * chore: remove NPM_TOKEN in .travis.yml
    
    * chore: remove node 8 in ci
    
    * chore: remove yarn test in ci

---
+++ typedoc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  out: 'docs',
+  includes: './',
+  exclude: ['src/index.ts', 'src/types/**'],
+  excludeExternals: true,
+  excludeNotExported: true,
+  excludePrivate: true,
+}
